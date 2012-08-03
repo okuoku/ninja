d693afd924f0d3cf882bff0af9dcf1abdc4775e8
Claus Klein <claus.klein@arcormail.de>
2012-08-03 17:59:18 +0200

    parse the version from header file and use only branch name

--- src/version.h
+++ src/version.h
@@ -1,1 +1,1 @@
-const char* kVersion = "ninja V0.1.3 ## develop-a94b1b2";
+const char* kVersion = "ninja V0.1.3 ## develop";
--- src/version.sh
+++ src/version.sh
@@ -14,26 +14,39 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Update the git branch name at version header if possible
+# It parse and update the c header file with following contents:
+#   const char* kVersion = "ninja V0.1.3 ## develop";
+# If the contents is not changed, the header file is not touched
+
 header="src/version.h"
-version="V0.1.3" 
 
-set -u
-set -e
-# set -x
+set -u  # undefined is an error
+set -e  # exit on error
+###XXX set -x   # be verbose
 
-which git || exit 0	# OK, noop
+type git || exit 0  # OK, noop
 
 if [ -d .git ]; then
-  branch=`git status -bsu no`
-  revisioncount=`git log --oneline | wc -l`
-  projectversion=`git describe --tags --long --always`
-  # generate
-  echo "const char* kVersion = \"ninja ${version} ${branch%%.*}-${projectversion%%-*}\";" \
+  # get the branch name from git
+  branch=$(git status -bsu no)
+  # try to get the most recent tag that is reachable from a git commit
+  projectversion=$(git describe --tags --dirty --always)
+
+  # parse the current release version from header file
+  version=$(awk '/kVersion/ {print $6}' "${header}")
+
+  # write a the temporary header file
+  echo "const char* kVersion = \"ninja ${version} ${branch%%.*}-${projectversion%%-*}\";"
+  echo "const char* kVersion = \"ninja ${version} ${branch%%.*}\";" \
     > "${header}.$$"
-  # diff and mv if needed
+
+  # diff with current header and mv if needed
   cmp -s "${header}.$$" "${header}" ||
     mv "${header}.$$" "${header}"
+
   # cleanup
   rm -f "${header}.$$"
+  cat "${header}"
 fi
 
